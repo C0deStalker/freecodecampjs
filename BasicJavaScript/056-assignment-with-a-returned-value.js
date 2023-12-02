5ccb9ce7662d584a2154fabf8a3230163e22964c
C0deStalker <dravida@mail.ru>
2023-12-03 01:06:27 +0300

    Ex036-056 fix naming error

---
+++ BasicJavaScript/056-assignment-with-a-returned-value.js
@@ -0,0 +1,20 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code below this line
+function processArg(number) {
+  return number - 5;
+}
+let processed = processArg(7);
+// Only change code above this line
+
+addThree();
+addFive();
